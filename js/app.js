e35354f1591234877117c3ae4f28098c907a9476
Klevtsov <sergei.klevtsov@accenture.com>
2017-05-11 16:54:45 +0300

    Refactoring

--- js/app.js
+++ js/app.js
@@ -10,25 +10,24 @@ const counter = (state = 0, action) => {
 };
 
 const createStore = (reducer) => {
-    let state;
-    let listeners = [];
+    
+    let state, listeners = [];
 
-    const getState = () => state; // returns state
-
-    const dispatch = (action) => {
-        state = reducer(state, action);
-        listeners.forEach(listener => listener());
-    };
-
-    const subscribe = (listener) => {
-        listeners.push(listener);
-        return () => {
-            listeners = listeners.filter(l => l !== listener);
-        }
-    };
+    const   getState = () => state,
+            
+            dispatch = (action) => {
+                state = reducer(state, action);
+                listeners.forEach(listener => listener());
+            },
+            
+            subscribe = (listener) => {
+                listeners.push(listener);
+                return () => {
+                    listeners = listeners.filter(l => l !== listener);
+                }
+            };
 
     dispatch({});
 
     return { getState, dispatch, subscribe };
-
 };
