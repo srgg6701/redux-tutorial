2dc34550e8143db24b834756e64c8691ad90999d
Klevtsov <sergei.klevtsov@accenture.com>
2017-05-11 15:36:37 +0300

    Turning subscribe callback into render function

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    var contentsBox = document.getElementById('contents');
+    const contentsBox = document.getElementById('contents');
     // 
     document.querySelectorAll('#commands a').forEach((link) => {
         link.addEventListener('click', () => {
@@ -10,7 +10,7 @@ window.onload = () => {
             console.log('act=>', act, 'state=>', store.getState());
         });
     });
-    store.subscribe(() => {
-        contentsBox.innerHTML =  'Current state: ' + store.getState();
-    });
+    const render = () => contentsBox.innerHTML =  'Current state: ' + store.getState();
+    
+    store.subscribe(render);
 }
