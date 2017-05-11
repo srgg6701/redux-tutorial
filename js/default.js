f79679b30412e5ca41929beebd3477cb4469e392
Klevtsov <sergei.klevtsov@accenture.com>
2017-05-11 14:51:41 +0300

    Set listeners to dispatch

--- js/default.js
+++ js/default.js
@@ -1,10 +1,10 @@
 window.onload = function(){
-    console.log('Redux, createStore, store=>', {Redux:Redux, createStore:createStore, store:store});
+    // 
     document.querySelectorAll('#commands a').forEach(function(link){
         link.addEventListener('click', function(){
             var act = this.id.toUpperCase()
             store.dispatch({type: act});
-            console.log('act=>', act);
+            console.log('act=>', act, 'state=>', store.getState());
         });
     });
 }
