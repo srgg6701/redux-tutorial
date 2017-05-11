29632e5ddc289a2f0e6d988d71fb59f5f3c84029
Klevtsov <sergei.klevtsov@accenture.com>
2017-05-11 16:57:19 +0300

    Small refactoring

--- js/default.js
+++ js/default.js
@@ -1,20 +1,18 @@
 window.onload = () => {
     
-    const contentsBox = document.getElementById('contents');
-
-    const store = createStore(counter); // returns store.getState, store.dispatch, store.subscribe
+    const   contentsBox = document.getElementById('contents'),
+            store = createStore(counter), // returns store.getState, store.dispatch, store.subscribe
+            render = () => contentsBox.innerHTML =  'Current state: ' + store.getState();
 
     // 
     document.querySelectorAll('#commands a').forEach((link) => {
         link.addEventListener('click', () => {
             // ! this is the window
-            console.log('link=>',link);
             var act = link.id.toUpperCase()
             store.dispatch({type: act});
             console.log('act=>', act, 'state=>', store.getState());
         });
     });
-    const render = () => contentsBox.innerHTML =  'Current state: ' + store.getState();
     
     store.subscribe(render);
 }
